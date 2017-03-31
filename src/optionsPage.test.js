8cfe0a3c81423d7728ff29b89cb112e84306b644
knoxzin1 <knoxzin1@gmail.com>
2017-03-30 21:01:01 -0300

    Updated tests

--- src/optionsPage.test.js
+++ src/optionsPage.test.js
@@ -1,153 +1,128 @@
-var path = require('path');
-var crdv = require('chromedriver');
-var webdriver = require('selenium-webdriver');
-var chai = require('chai');
-var assert = chai.assert;
+const path = require('path');
+const crdv = require('chromedriver');
+const {Builder, By, promise, until} = require('selenium-webdriver');
+const chai = require('chai');
+const assert = chai.assert;
 
-var extensionPath = path.resolve('./build/chrome');
-var extensionId = 'jbbplengggjdnlghliebnhfbemmfmcjd';
-var optionsPage = `chrome-extension://${extensionId}/src/options.html`;
+const extensionPath = path.resolve('./build/chrome');
+const extensionId = 'jbbplengggjdnlghliebnhfbemmfmcjd';
+const optionsPage = `chrome-extension://${extensionId}/src/options.html`;
 
-var driver = null;
+let driver = null;
+
+promise.USE_PROMISE_MANAGER = false;
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 describe('src/optionsPage.js', function() {
   this.timeout(50000);
 
-  before(function(done) {
+  before(async function() {
     crdv.start();
 
-    setTimeout(function() {
+    await timeout(2000);
 
-      driver = new webdriver.Builder()
-        .usingServer('http://localhost:9515')
-        .withCapabilities({
-          chromeOptions: {
-            args: [`load-extension=${extensionPath}`]
-          }
-        })
-        .forBrowser('chrome')
-        .build();
+    driver = await new Builder()
+      .usingServer('http://localhost:9515')
+      .withCapabilities({
+        chromeOptions: {
+          args: [`load-extension=${extensionPath}`]
+        }
+      })
+      .forBrowser('chrome')
+      .build();
 
-      done();
-    }, 2000);
+    driver.manage().timeouts().setScriptTimeout(4000);
   });
 
-  after(function(done) {
-    driver.quit()
-      .then(function() {
-        crdv.stop();
-      })
-      .then(done);
+  after(async function() {
+    await driver.quit();
+    crdv.stop();
   });
 
-  it('should open extension options window', function(done) {
-    driver.get(optionsPage)
-      .then(function() {
-        return driver.getCurrentUrl();
-      })
-      .then(function(url) {
-        assert.deepEqual(url, optionsPage);
-      })
-      .then(done)
-      .catch(done);
+  it('should open extension options window', async function() {
+    await driver.get(optionsPage);
+    const url = await driver.getCurrentUrl();
+    assert.deepEqual(url, optionsPage);
   });
 
-  it('should contain the form options', function() {
+  it('should contain the form options', async function() {
     // selenium will throw if the element is not present
-    driver.findElement(webdriver.By.name('dontScrollPosts'));
-    driver.findElement(webdriver.By.name('dontPlayGifs'));
+    await driver.findElement(By.name('dontScrollPosts'));
+    await driver.findElement(By.name('dontPlayGifs'));
   });
 
-  it('should save "dontScrollPosts" to chrome.storage', function(done) {
-    driver.manage().timeouts().setScriptTimeout(2000);
-    var dontScrollPosts = driver.findElement(webdriver.By.name('dontScrollPosts'));
-    dontScrollPosts.click();
+  it('should save "dontScrollPosts" to chrome.storage', async function() {
+    let dontScrollPosts = await driver.findElement(By.name('dontScrollPosts'));
 
-    driver.executeAsyncScript(function() {
+    await dontScrollPosts.click();
+    await timeout(200);
+    const chk1 = await driver.executeAsyncScript(function() {
       var callback = arguments[arguments.length - 1];
       chrome.storage.sync.get('dontScrollPosts', function(item) {
         callback(item);
       });
-    })
-    .then(function(item) {
-      assert.propertyVal(item, 'dontScrollPosts', true, 'should be true after first click');
-    })
-    .then(function() {
-      dontScrollPosts.click();
+    });
+    assert.propertyVal(chk1, 'dontScrollPosts', true, 'should be true after first click');
 
-      return driver.executeAsyncScript(function() {
-        var callback = arguments[arguments.length - 1];
-        chrome.storage.sync.get('dontScrollPosts', function(item) {
-          callback(item);
-        });
+    await dontScrollPosts.click();
+    await timeout(200);
+    const chk2 = await driver.executeAsyncScript(function() {
+      var callback = arguments[arguments.length - 1];
+      chrome.storage.sync.get('dontScrollPosts', function(item) {
+        callback(item);
       });
-    })
-    .then(function(item) {
-      assert.propertyVal(item, 'dontScrollPosts', false, 'should be false after second click');
-    })
-    .then(done)
-    .catch(done);
+    });
+    assert.propertyVal(chk2, 'dontScrollPosts', false, 'should be false after second click');
   });
 
-  it('should save "dontPlayGifs" to chrome.storage', function(done) {
-    driver.manage().timeouts().setScriptTimeout(2000);
-    var dontPlayGifs = driver.findElement(webdriver.By.name('dontPlayGifs'));
-    dontPlayGifs.click();
+  it('should save "dontPlayGifs" to chrome.storage', async function() {
+    let dontPlayGifs = await driver.findElement(By.name('dontPlayGifs'));
 
-    driver.executeAsyncScript(function() {
+    await dontPlayGifs.click();
+    await timeout(200);
+    const chk1 = await driver.executeAsyncScript(function() {
       var callback = arguments[arguments.length - 1];
       chrome.storage.sync.get('dontPlayGifs', function(item) {
         callback(item);
       });
     })
-    .then(function(item) {
-      assert.propertyVal(item, 'dontPlayGifs', true, 'should be true after first click');
-    })
-    .then(function() {
-      dontPlayGifs.click();
+    assert.propertyVal(chk1, 'dontPlayGifs', true, 'should be true after first click');
 
-      return driver.executeAsyncScript(function() {
-        var callback = arguments[arguments.length - 1];
-        chrome.storage.sync.get('dontPlayGifs', function(item) {
-          callback(item);
-        });
+    await dontPlayGifs.click();
+    await timeout(200);
+    const chk2 = await driver.executeAsyncScript(function() {
+      var callback = arguments[arguments.length - 1];
+      chrome.storage.sync.get('dontPlayGifs', function(item) {
+        callback(item);
       });
-    })
-    .then(function(item) {
-      assert.propertyVal(item, 'dontPlayGifs', false, 'should be false after second click');
-    })
-    .then(done)
-    .catch(done);
+    });
+    assert.propertyVal(chk2, 'dontPlayGifs', false, 'should be false after second click');
   });
 
-  it('should save "disableMarkdown" to chrome.storage', function(done) {
-    driver.manage().timeouts().setScriptTimeout(2000);
-    var disableMarkdown = driver.findElement(webdriver.By.name('disableMarkdown'));
-    disableMarkdown.click();
+  it('should save "disableMarkdown" to chrome.storage', async function() {
+    let disableMarkdown = await driver.findElement(By.name('disableMarkdown'));
 
-    driver.executeAsyncScript(function() {
+    await disableMarkdown.click();
+    await timeout(200);
+    const chk1 = await driver.executeAsyncScript(function() {
       var callback = arguments[arguments.length - 1];
       chrome.storage.sync.get('disableMarkdown', function(item) {
         callback(item);
       });
     })
-    .then(function(item) {
-      assert.propertyVal(item, 'disableMarkdown', true, 'should be true after first click');
-    })
-    .then(function() {
-      disableMarkdown.click();
+    assert.propertyVal(chk1, 'disableMarkdown', true, 'should be true after first click');
 
-      return driver.executeAsyncScript(function() {
-        var callback = arguments[arguments.length - 1];
-        chrome.storage.sync.get('disableMarkdown', function(item) {
-          callback(item);
-        });
+    await disableMarkdown.click();
+    await timeout(200);
+    const chk2 = await driver.executeAsyncScript(function() {
+      var callback = arguments[arguments.length - 1];
+      chrome.storage.sync.get('disableMarkdown', function(item) {
+        callback(item);
       });
-    })
-    .then(function(item) {
-      assert.propertyVal(item, 'disableMarkdown', false, 'should be false after second click');
-    })
-    .then(done)
-    .catch(done);
+    });
+    assert.propertyVal(chk2, 'disableMarkdown', false, 'should be false after second click');
   });
 });
--- src/pageScript.test.js
+++ src/pageScript.test.js
@@ -1,135 +1,105 @@
-var path = require('path');
-var crdv = require('chromedriver');
-var webdriver = require('selenium-webdriver');
-var chai = require('chai');
-var nconf = require('nconf');
-var assert = chai.assert;
+const path = require('path');
+const crdv = require('chromedriver');
+const {Builder, By, promise, until} = require('selenium-webdriver');
+const chai = require('chai');
+const nconf = require('nconf');
+const assert = chai.assert;
 
-var extensionPath = path.resolve('./build/chrome');
-var driver = null;
+const extensionPath = path.resolve('./build/chrome');
+let driver = null;
 
 nconf.argv()
   .env('_')
   .file({ file: './config.json' });
 
+promise.USE_PROMISE_MANAGER = false;
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 describe('src/pageScript.js', function() {
-  this.timeout(200000);
+  this.timeout(50000);
 
-  before(function(done) {
+  before(async function() {
     crdv.start();
 
-    setTimeout(function() {
+    await timeout(2000);
 
-      driver = new webdriver.Builder()
-        .usingServer('http://localhost:9515')
-        .withCapabilities({
-          chromeOptions: {
-            args: [`load-extension=${extensionPath}`]
-          }
-        })
-        .forBrowser('chrome')
-        .build();
+    driver = await new Builder()
+      .usingServer('http://localhost:9515')
+      .withCapabilities({
+        chromeOptions: {
+          args: [`load-extension=${extensionPath}`]
+        }
+      })
+      .forBrowser('chrome')
+      .build();
 
-      // LOGIN
-      driver.get('https://vk.com/')
-        .then(function() {
-          var $email = driver.findElement(webdriver.By.id('index_email'));
-          var $pass = driver.findElement(webdriver.By.id('index_pass'));
-          var $submit = driver.findElement(webdriver.By.id('index_login_button'));
+    // LOGIN
+    await driver.get('https://vk.com/');
 
-          $email.sendKeys(nconf.get('vkManager:testLogin'));
-          $pass.sendKeys(nconf.get('vkManager:testPassword'));
+    let $email = await driver.findElement(By.id('index_email'));
+    let $pass = await driver.findElement(By.id('index_pass'));
+    let $submit = await driver.findElement(By.id('index_login_button'));
 
-          $submit.click();
+    await $email.sendKeys(nconf.get('vkManager:testLogin'));
+    await $pass.sendKeys(nconf.get('vkManager:testPassword'));
+    await $submit.click();
 
-          driver.sleep(10000);
+    await driver.sleep(10000);
 
-          return driver.getTitle();
-        })
-        .then((title) => {
-          console.log(title);
-          if (title.trim() === 'Security Check') {
-            var $code = driver.findElement(webdriver.By.id('code'));
-            var $submit = driver.findElement(webdriver.By.id('validate_btn'));
+    const title = await driver.getTitle();
 
-            $code.sendKeys(nconf.get('vkManager:testPhone'));
-            $submit.click();
-          }
+    if (title.trim() === 'Security Check') {
+      let $securityCode = await driver.findElement(By.id('code'));
+      let $securitySubmit = await driver.findElement(By.id('validate_btn'));
 
-          driver.wait(webdriver.until.titleIs('News'), 10000);
-        })
-        .then(done)
-        .catch(function() {
-          driver.getCurrentUrl().then(console.log.bind(console));
-          driver.getTitle().then(console.log.bind(console));
-          done();
-        });
-    }, 2000);
+      await $securityCode.sendKeys(nconf.get('vkManager:testPhone'));
+      await $securitySubmit.click();
+    }
+
+    await driver.wait(until.titleIs('News'), 10000);
   });
 
-  after(function(done) {
-    driver.quit()
-      .then(function() {
-        crdv.stop();
-      })
-      .then(done);
+  after(async function() {
+    await driver.quit();
+    crdv.stop();
   });
 
-  it('should change the last board crumb to a anchor', function(done) {
-    driver.get(nconf.get('vkManager:boardLink'));
-    driver.wait(webdriver.until.elementLocated(webdriver.By.css('a.ui_crumb:last-child')), 10000);
-    var $discussionBoard = driver.findElement(webdriver.By.css('a.ui_crumb:last-child'));
-    driver.wait(webdriver.until.elementTextContains($discussionBoard, 'Discussion board'), 10000);
-
-    done();
+  it('should change the last board crumb to a anchor', async function() {
+    await driver.get(nconf.get('vkManager:boardLink'));
+    await driver.wait(until.elementLocated(By.css('a.ui_crumb:last-child')), 10000);
+    var $discussionBoard = await driver.findElement(By.css('a.ui_crumb:last-child'));
+    await driver.wait(until.elementTextContains($discussionBoard, 'Discussion board'), 10000);
   });
 
-  it('should change the second crumb onClick attribute', function(done) {
-    driver.get(nconf.get('vkManager:topicLink'));
+  it('should change the second crumb onClick attribute', async function() {
+    await driver.get(nconf.get('vkManager:topicLink'));
 
-    driver.findElement(webdriver.By.css('a.ui_crumb:nth-of-type(2)'))
-      .getAttribute('onclick')
-      .then(function(attr) {
-        assert.deepEqual(attr, 'return nav.go(this, event);');
-      })
-      .then(done)
-      .catch(done);
+    const attr = await driver.findElement(By.css('a.ui_crumb:nth-of-type(2)'))
+                .getAttribute('onclick');
+
+    assert.deepEqual(attr, 'return nav.go(this, event);');
   });
 
-  it('should play all gifs showing on the page', function(done) {
-    driver.get(nconf.get('vkManager:topicLink'))
-      .then(function() {
-        return driver.findElements(webdriver.By.css('.photo.page_doc_photo_href'));
-      })
-      .then(function(docs) {
-        var promises = docs.map(function(doc) {
-          return !doc.isDisplayed();
-        });
+  it('should play all gifs showing on the page', async function() {
+    await driver.get(nconf.get('vkManager:topicLink'));
 
-        return Promise.all(promises);
-      })
-      .then(function(isDisplayedArr) {
-        isDisplayedArr.forEach(function(isDisplayed) {
-          assert.isFalse(isDisplayed, 'gif thumb should not be visible');
-        });
-      })
-      .then(function() {
-        return driver.findElements(webdriver.By.css('.page_gif_preview.page_gif_loaded'));
-      })
-      .then(function(gifs) {
-        var promises = gifs.map(function(gif) {
-          return gif.isDisplayed();
-        });
+    const docs = await driver.findElements(By.css('.photo.page_doc_photo_href'));
+    const isDisplayedArr = await Promise.all(docs.map(function(doc) {
+      return !doc.isDisplayed();
+    }));
+    isDisplayedArr.forEach(function(isDisplayed) {
+      assert.isFalse(isDisplayed, 'gif thumb should not be visible');
+    });
 
-        return Promise.all(promises);
-      })
-      .then(function(isDisplayedArr) {
-        isDisplayedArr.forEach(function(isDisplayed) {
-          assert.isTrue(isDisplayed, 'gif should be visible');
-        });
-      })
-      .then(done)
-      .catch(done);
+    const gifs = await driver.findElements(By.css('.page_gif_preview.page_gif_loaded'));
+    const gifsAreDisplayed = await Promise.all(gifs.map(function(gif) {
+      return gif.isDisplayed();
+    }));
+    gifsAreDisplayed.forEach(function(isDisplayed) {
+      assert.isTrue(isDisplayed, 'gif should be visible');
+    });
   });
 });
